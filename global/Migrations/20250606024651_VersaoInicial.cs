f83dc5a4bb0792198cc001ecd99197fcd4660cca
Caroline Oliveira <carolinedeoliveira.dev@gmail.com>
2025-09-02 10:56:12 -0300

    Update: substituir literais duplicados 'NUMBER(10)' por constante

--- global/Migrations/20250606024651_VersaoInicial.cs
+++ global/Migrations/20250606024651_VersaoInicial.cs
@@ -1,21 +1,21 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace global.Migrations
 {
-    /// <inheritdoc />
     public partial class VersaoInicial : Migration
     {
-        /// <inheritdoc />
+        private const string OracleNumber10 = "NUMBER(10)";
+
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
                 name: "LOG_API",
                 columns: table => new
                 {
-                    ID_LOG = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                    ID_LOG = table.Column<int>(type: OracleNumber10, nullable: false)
                         .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
                     TIPO = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
                     ENDPOINT = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
@@ -32,7 +32,7 @@ namespace global.Migrations
                 name: "PREVISAO_TEMPO",
                 columns: table => new
                 {
-                    ID_PREVISAO_TEMPO = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                    ID_PREVISAO_TEMPO = table.Column<int>(type: OracleNumber10, nullable: false)
                         .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
                     LOCALIZACAO = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
                     DATA_HORA = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: true),
@@ -51,7 +51,7 @@ namespace global.Migrations
                 name: "SENSORES",
                 columns: table => new
                 {
-                    ID_SENSOR = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                    ID_SENSOR = table.Column<int>(type: OracleNumber10, nullable: false)
                         .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
                     NOME = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
                     LOCALIZACAO_SENSOR = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true),
@@ -67,7 +67,7 @@ namespace global.Migrations
                 name: "USUARIOS",
                 columns: table => new
                 {
-                    ID_USUARIO = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                    ID_USUARIO = table.Column<int>(type: OracleNumber10, nullable: false)
                         .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
                     NOME = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
                     EMAIL = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
@@ -83,9 +83,9 @@ namespace global.Migrations
                 name: "LEITURA_SENSOR",
                 columns: table => new
                 {
-                    ID_LEITURA_SENSOR = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                    ID_LEITURA_SENSOR = table.Column<int>(type: OracleNumber10, nullable: false)
                         .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
-                    ID_SENSOR = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    ID_SENSOR = table.Column<int>(type: OracleNumber10, nullable: false),
                     NIVEL_AGUA = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true),
                     DATA_HORA = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: true)
                 },
@@ -104,11 +104,11 @@ namespace global.Migrations
                 name: "ALERTA",
                 columns: table => new
                 {
-                    ID_ALERTA = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                    ID_ALERTA = table.Column<int>(type: OracleNumber10, nullable: false)
                         .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
-                    ID_USUARIO = table.Column<int>(type: "NUMBER(10)", nullable: true),
-                    ID_SENSOR = table.Column<int>(type: "NUMBER(10)", nullable: true),
-                    ID_PREVISAO_TEMPO = table.Column<int>(type: "NUMBER(10)", nullable: true),
+                    ID_USUARIO = table.Column<int>(type: OracleNumber10, nullable: true),
+                    ID_SENSOR = table.Column<int>(type: OracleNumber10, nullable: true),
+                    ID_PREVISAO_TEMPO = table.Column<int>(type: OracleNumber10, nullable: true),
                     TIPO_ALERTA = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
                     MENSAGEM = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
                     DATA_HORA = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: true),
@@ -118,63 +118,31 @@ namespace global.Migrations
                 {
                     table.PrimaryKey("PK_ALERTA", x => x.ID_ALERTA);
                     table.ForeignKey(
-                        name: "FK_ALERTA_PREVISAO_TEMPO_ID_PREVISAO_TEMPO",
-                        column: x => x.ID_PREVISAO_TEMPO,
-                        principalTable: "PREVISAO_TEMPO",
-                        principalColumn: "ID_PREVISAO_TEMPO");
+                        name: "FK_ALERTA_USUARIOS_ID_USUARIO",
+                        column: x => x.ID_USUARIO,
+                        principalTable: "USUARIOS",
+                        principalColumn: "ID_USUARIO");
                     table.ForeignKey(
                         name: "FK_ALERTA_SENSORES_ID_SENSOR",
                         column: x => x.ID_SENSOR,
                         principalTable: "SENSORES",
                         principalColumn: "ID_SENSOR");
                     table.ForeignKey(
-                        name: "FK_ALERTA_USUARIOS_ID_USUARIO",
-                        column: x => x.ID_USUARIO,
-                        principalTable: "USUARIOS",
-                        principalColumn: "ID_USUARIO");
+                        name: "FK_ALERTA_PREVISAO_TEMPO_ID_PREVISAO_TEMPO",
+                        column: x => x.ID_PREVISAO_TEMPO,
+                        principalTable: "PREVISAO_TEMPO",
+                        principalColumn: "ID_PREVISAO_TEMPO");
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ALERTA_ID_PREVISAO_TEMPO",
-                table: "ALERTA",
-                column: "ID_PREVISAO_TEMPO");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ALERTA_ID_SENSOR",
-                table: "ALERTA",
-                column: "ID_SENSOR");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ALERTA_ID_USUARIO",
-                table: "ALERTA",
-                column: "ID_USUARIO");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_LEITURA_SENSOR_ID_SENSOR",
-                table: "LEITURA_SENSOR",
-                column: "ID_SENSOR");
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "ALERTA");
-
-            migrationBuilder.DropTable(
-                name: "LEITURA_SENSOR");
-
-            migrationBuilder.DropTable(
-                name: "LOG_API");
-
-            migrationBuilder.DropTable(
-                name: "PREVISAO_TEMPO");
-
-            migrationBuilder.DropTable(
-                name: "USUARIOS");
-
-            migrationBuilder.DropTable(
-                name: "SENSORES");
+            migrationBuilder.DropTable(name: "ALERTA");
+            migrationBuilder.DropTable(name: "LEITURA_SENSOR");
+            migrationBuilder.DropTable(name: "LOG_API");
+            migrationBuilder.DropTable(name: "PREVISAO_TEMPO");
+            migrationBuilder.DropTable(name: "USUARIOS");
+            migrationBuilder.DropTable(name: "SENSORES");
         }
     }
 }
